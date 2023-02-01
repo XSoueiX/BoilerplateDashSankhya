48f4e977e15c66648274a122a1fbf2ea0325d32f
XSoueiX <paulo2016rezende@gmail.com>
2023-02-01 15:04:46 -0300

    primeito up

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,14 @@ var app = angular.module('myApp', ['moduloTeste', 'ngMessages']);
 
 
 app.controller('myCtrl', function ($scope, $q, customUtils) {
+    let select = 'FORN.CODPARC , PAR.RAZAOSOCIAL, SUM(FORN.TOTALVLR) / COUNT(1) AS MEDIA';
+    let from = 'AD_TGEAVALFORN FORN INNER JOIN TGFPAR PAR ON FORN.CODPARC = PAR.CODPARC';
+    let where = '1=1 GROUP BY FORN.CODPARC, PAR.RAZAOSOCIAL ORDER BY MEDIA, PAR.RAZAOSOCIAL';
+   
+    let p = customUtils.doRequest(select,from,where,true,'DADOS');
 
+    p.then((result) => {
+        debugger;
+    })
 
 });
